bc4c0777721bafc250ed9a40526521e91dab39d4
AlejandroAlvis <sergio.alvis@campusucc.edu.co>
2019-08-30 19:05:18 -0500

    Initial commit

---
+++ src/clienteservidor/MainCliente.java
@@ -0,0 +1,19 @@
+package clienteservidor;
+
+import java.io.IOException;
+
+/*
+ * @author Alejandro Alvis
+ */
+public class MainCliente 
+{
+    
+    public static void main(String[] args) throws IOException
+    {
+        Cliente cli = new Cliente(); //Se crea el cliente
+
+        System.out.println("Iniciando Cliente\n");
+        cli.startClient(); //Se inicia el cliente
+    }
+    
+}
